fff5d896b1b503adfdaddba30225707c3c18af8a
Jose Antonio Alvarez Cubero <jaacubero@gmail.com>
2022-03-27 14:11:01 +0200

    New version

--- docs/conf.py
+++ docs/conf.py
@@ -226,7 +226,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'indiceclimabook', u'Indice de cambio climatico'',
+    ('index', 'indiceclimabook', u'Indice de cambio climatico',
      [u'Jose Antonio Alvarez Cubero'], 1)
 ]
 
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'indiceclimabook', u'Indice de cambio climatico'',
-   u'Indice de cambio climatico'', 'Jose Antonio Alvarez Cubero', 'Indice de cambio climatico'.',
+  ('index', 'indiceclimabook', u'Indice de cambio climatico',
+   u'Indice de cambio climatico', 'Jose Antonio Alvarez Cubero', 'Indice de cambio climatico'.',
    'Miscellaneous'),
 ]
 
